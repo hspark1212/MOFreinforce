5753e97d2dc0662b6e61dc88f224fe11157c81ac
hspark1212 <phs68660888@gmail.com>
2023-02-22 11:19:47 +0900

    230222 3

---
+++ mofreinforce/libs/selfies/encoder.py
@@ -0,0 +1,250 @@
+# This code is adapted from the "selfies" repository:
+# https://github.com/aspuru-guzik-group/selfies.git
+
+# The code in this file is licensed under the Apache License, Version 2.0:
+# https://github.com/aspuru-guzik-group/selfies/blob/master/LICENSE
+
+# Adapted by Hyunsoo Park, 2022
+
+from .exceptions import EncoderError, SMILESParserError
+from .grammar_rules import get_selfies_from_index
+from .utils.smiles_utils import (
+    atom_to_smiles,
+    bond_to_smiles,
+    smiles_to_mol
+)
+
+from .mol_graph import AttributionMap
+
+
+def encoder(smiles: str, strict: bool = True, attribute: bool = False) -> str:
+    """Translates a SMILES string into its corresponding SELFIES string.
+
+    This translation is deterministic and does not depend on the
+    current semantic constraints. Additionally, it preserves the atom order
+    of the input SMILES string; thus, one could generate randomized SELFIES
+    strings by generating randomized SMILES strings, and then translating them.
+
+    By nature of SELFIES, it is impossible to represent molecules that
+    violate the current semantic constraints as SELFIES strings.
+    Thus, we provide the ``strict`` flag to guard against such cases. If
+    ``strict=True``, then this function will raise a
+    :class:`selfies.EncoderError` if the input SMILES string represents
+    a molecule that violates the semantic constraints. If
+    ``strict=False``, then this function will not raise any error; however,
+    calling :func:`selfies.decoder` on a SELFIES string generated this
+    way will *not* be guaranteed to recover a SMILES string representing
+    the original molecule.
+
+    :param smiles: the SMILES string to be translated. It is recommended to
+        use RDKit to check that the strings passed into this function
+        are valid SMILES strings.
+    :param strict: if ``True``, this function will check that the
+        input SMILES string obeys the semantic constraints.
+        Defaults to ``True``.
+    :param attribute: if an attribution should be returned
+    :return: a SELFIES string translated from the input SMILES string if
+             attribute is ``False``, otherwise a tuple is returned of
+             SELFIES string and attribution list.
+    :raises EncoderError:  if the input SMILES string is invalid,
+        cannot be kekulized, or violates the semantic constraints with
+        ``strict=True``.
+
+    :Example:
+
+    >>> import selfies as sf
+    >>> sf.encoder("C=CF")
+    '[C][=C][F]'
+
+    .. note:: This function does not currently support SMILES with:
+
+        *   The wildcard symbol ``*``.
+        *   The quadruple bond symbol ``$``.
+        *   Chirality specifications other than ``@`` and ``@@``.
+        *   Ring bonds across a dot symbol (e.g. ``c1cc([O-].[Na+])ccc1``) or
+            ring bonds between atoms that are over 4000 atoms apart.
+
+        Although SELFIES does not have aromatic symbols, this function
+        *does* support aromatic SMILES strings by internally kekulizing them
+        before translation.
+    """
+
+    try:
+        mol = smiles_to_mol(smiles, attributable=attribute)
+    except SMILESParserError as err:
+        err_msg = "failed to parse input\n\tSMILES: {}".format(smiles)
+        raise EncoderError(err_msg) from err
+
+    if not mol.kekulize():
+        err_msg = "kekulization failed\n\tSMILES: {}".format(smiles)
+        raise EncoderError(err_msg)
+
+    if strict:
+        _check_bond_constraints(mol, smiles)
+
+    # invert chirality of atoms where necessary,
+    # such that they are restored when the SELFIES is decoded
+    for atom in mol.get_atoms():
+        if ((atom.chirality is not None)
+                and mol.has_out_ring_bond(atom.index)
+                and _should_invert_chirality(mol, atom)):
+            atom.invert_chirality()
+
+    fragments = []
+    attribution_maps = []
+    attribution_index = 0
+    for root in mol.get_roots():
+        derived = list(_fragment_to_selfies(
+            mol, None, root, attribution_maps, attribution_index))
+        attribution_index += len(derived)
+        fragments.append("".join(derived))
+    # trim attribution map of empty tokens
+    attribution_maps = [a for a in attribution_maps if a.token]
+    result = ".".join(fragments), attribution_maps
+    return result if attribute else result[0]
+
+
+def _check_bond_constraints(mol, smiles):
+    errors = []
+
+    for atom in mol.get_atoms():
+        bond_cap = atom.bonding_capacity
+        bond_count = mol.get_bond_count(atom.index)
+        if bond_count > bond_cap:
+            errors.append((atom_to_smiles(atom), bond_count, bond_cap))
+
+    if errors:
+        err_msg = "input violates the currently-set semantic constraints\n" \
+                  "\tSMILES: {}\n" \
+                  "\tErrors:\n".format(smiles)
+        for e in errors:
+            err_msg += "\t[{:} with {} bond(s) - " \
+                       "a max. of {} bond(s) was specified]\n".format(*e)
+        raise EncoderError(err_msg)
+
+
+def _should_invert_chirality(mol, atom):
+    out_bonds = mol.get_out_dirbonds(atom.index)
+
+    # 1. rings whose right number are bonded to this atom (e.g. ...1...X1)
+    # 2. rings whose left number are bonded to this atom (e.g. X1...1...)
+    # 3. branches and other (e.g. X(...)...)
+    partition = [[], [], []]
+    for i, bond in enumerate(out_bonds):
+        if not bond.ring_bond:
+            partition[2].append(i)
+        elif bond.src < bond.dst:
+            partition[1].append(i)
+        else:
+            partition[0].append(i)
+    partition[1].sort(key=lambda x: out_bonds[x].dst)
+
+    # construct permutation
+    perm = partition[0] + partition[1] + partition[2]
+    count = 0
+    for i in range(len(perm)):
+        for j in range(i + 1, len(perm)):
+            if perm[i] > perm[j]:
+                count += 1
+    return count % 2 != 0  # if odd permutation, should invert chirality
+
+
+def _fragment_to_selfies(mol, bond_into_root, root,
+                         attribution_maps, attribution_index=0):
+    derived = []
+
+    bond_into_curr, curr = bond_into_root, root
+    while True:
+        curr_atom = mol.get_atom(curr)
+        token = _atom_to_selfies(bond_into_curr, curr_atom)
+        derived.append(token)
+
+        attribution_maps.append(AttributionMap(
+            len(derived) - 1 + attribution_index,
+            token, mol.get_attribution(curr_atom)))
+
+        out_bonds = mol.get_out_dirbonds(curr)
+        for i, bond in enumerate(out_bonds):
+
+            if bond.ring_bond:
+                if bond.src < bond.dst:
+                    continue
+
+                rev_bond = mol.get_dirbond(src=bond.dst, dst=bond.src)
+                ring_len = bond.src - bond.dst
+                Q_as_symbols = get_selfies_from_index(ring_len - 1)
+                ring_symbol = "[{}Ring{}]".format(
+                    _ring_bonds_to_selfies(rev_bond, bond),
+                    len(Q_as_symbols)
+                )
+
+                derived.append(ring_symbol)
+                attribution_maps.append(AttributionMap(
+                    len(derived) - 1 + attribution_index,
+                    ring_symbol, mol.get_attribution(bond)))
+                for symbol in Q_as_symbols:
+                    derived.append(symbol)
+                    attribution_maps.append(AttributionMap(
+                        len(derived) - 1 + attribution_index,
+                        symbol, mol.get_attribution(bond)))
+
+            elif i == len(out_bonds) - 1:
+                bond_into_curr, curr = bond, bond.dst
+
+            else:
+                # start, end are so we can go back and
+                # correct offset from branch symbol in
+                # branch tokens
+                start = len(attribution_maps)
+                branch = _fragment_to_selfies(
+                    mol, bond, bond.dst, attribution_maps, len(derived))
+                Q_as_symbols = get_selfies_from_index(len(branch) - 1)
+                branch_symbol = "[{}Branch{}]".format(
+                    _bond_to_selfies(bond, show_stereo=False),
+                    len(Q_as_symbols)
+                )
+                end = len(attribution_maps)
+
+                derived.append(branch_symbol)
+                for symbol in Q_as_symbols:
+                    derived.append(symbol)
+                    attribution_maps.append(AttributionMap(
+                        len(derived) - 1 + attribution_index,
+                        symbol, mol.get_attribution(bond)))
+
+                # account for branch symbol because it is inserted after
+                for j in range(start, end):
+                    attribution_maps[j].index += len(Q_as_symbols) + 1
+                attribution_maps.append(AttributionMap(
+                    len(derived) - 1 + attribution_index,
+                    branch_symbol, mol.get_attribution(bond)))
+
+                derived.extend(branch)
+
+        # end of chain
+        if (not out_bonds) or out_bonds[-1].ring_bond:
+            break
+    return derived
+
+
+def _bond_to_selfies(bond, show_stereo=True):
+    if not show_stereo and (bond.order == 1):
+        return ""
+    return bond_to_smiles(bond)
+
+
+def _ring_bonds_to_selfies(lbond, rbond):
+    assert lbond.order == rbond.order
+
+    if (lbond.order != 1) or all(b.stereo is None for b in (lbond, rbond)):
+        return _bond_to_selfies(lbond, show_stereo=False)
+    else:
+        bond_char = "-" if (lbond.stereo is None) else lbond.stereo
+        bond_char += "-" if (rbond.stereo is None) else rbond.stereo
+        return bond_char
+
+
+def _atom_to_selfies(bond, atom):
+    assert not atom.is_aromatic
+    bond_char = "" if (bond is None) else _bond_to_selfies(bond)
+    return "[{}{}]".format(bond_char, atom_to_smiles(atom, brackets=False))
---
+++ mofreinforce/libs/selfies/grammar_rules.py
@@ -0,0 +1,216 @@
+# This code is adapted from the "selfies" repository:
+# https://github.com/aspuru-guzik-group/selfies.git
+
+# The code in this file is licensed under the Apache License, Version 2.0:
+# https://github.com/aspuru-guzik-group/selfies/blob/master/LICENSE
+
+# Adapted by Hyunsoo Park, 2022
+
+import functools
+import itertools
+import re
+from typing import Any, List, Optional, Tuple
+
+from .constants import (
+    ELEMENTS,
+    INDEX_ALPHABET,
+    INDEX_CODE,
+    ORGANIC_SUBSET
+)
+from .mol_graph import Atom
+from .utils.smiles_utils import smiles_to_bond
+
+
+def process_atom_symbol(symbol: str) -> Optional[Tuple[Any, Atom]]:
+    try:
+        output = _PROCESS_ATOM_CACHE[symbol]
+    except KeyError:
+        output = _process_atom_selfies_no_cache(symbol)
+        if output is None:
+            return None
+        _PROCESS_ATOM_CACHE[symbol] = output
+
+    bond_info, atom_fac = output
+    atom = atom_fac()
+    if atom.bonding_capacity < 0:
+        return None  # too many Hs (e.g. [CH9]
+    return bond_info, atom
+
+
+def process_branch_symbol(symbol: str) -> Optional[Tuple[int, int]]:
+    try:
+        return _PROCESS_BRANCH_CACHE[symbol]
+    except KeyError:
+        return None
+
+
+def process_ring_symbol(symbol: str) -> Optional[Tuple[int, int, Any]]:
+    try:
+        return _PROCESS_RING_CACHE[symbol]
+    except KeyError:
+        return None
+
+
+def next_atom_state(
+        bond_order: int, bond_cap: int, state: int
+) -> Tuple[int, Optional[int]]:
+    if state == 0:
+        bond_order = 0
+
+    bond_order = min(bond_order, state, bond_cap)
+    bonds_left = bond_cap - bond_order
+    next_state = None if (bonds_left == 0) else bonds_left
+    return bond_order, next_state
+
+
+def next_branch_state(
+        branch_type: int, state: int
+) -> Tuple[int, Optional[int]]:
+    assert 1 <= branch_type <= 3
+    assert state > 1
+
+    branch_init_state = min(state - 1, branch_type)
+    next_state = state - branch_init_state
+    return branch_init_state, next_state
+
+
+def next_ring_state(
+        ring_type: int, state: int
+) -> Tuple[int, Optional[int]]:
+    assert state > 0
+
+    bond_order = min(ring_type, state)
+    bonds_left = state - bond_order
+    next_state = None if (bonds_left == 0) else bonds_left
+    return bond_order, next_state
+
+
+def get_index_from_selfies(*symbols: List[str]) -> int:
+    index = 0
+    for i, c in enumerate(reversed(symbols)):
+        index += INDEX_CODE.get(c, 0) * (len(INDEX_CODE) ** i)
+    return index
+
+
+def get_selfies_from_index(index: int) -> List[str]:
+    if index < 0:
+        raise IndexError()
+    elif index == 0:
+        return [INDEX_ALPHABET[0]]
+
+    symbols = []
+    base = len(INDEX_ALPHABET)
+    while index:
+        symbols.append(INDEX_ALPHABET[index % base])
+        index //= base
+    return symbols[::-1]
+
+
+# =============================================================================
+# Caches (for computational speed)
+# =============================================================================
+
+
+SELFIES_ATOM_PATTERN = re.compile(
+    r"^[\[]"  # opening square bracket [
+    r"([=#/\\]?)"  # bond char
+    r"(\d*)"  # isotope number (optional, e.g. 123, 26)
+    r"([A-Z][a-z]?|\*)"  # element symbol
+    r"([@]{0,2})"  # chiral_tag (optional, only @ and @@ supported)
+    r"((?:[H]\d)?)"  # H count (optional, e.g. H1, H3)
+    r"((?:[+-][1-9]+)?)"  # charge (optional, e.g. +1)
+    r"[]]$"  # closing square bracket ]
+)
+
+
+def _process_atom_selfies_no_cache(symbol):
+    m = SELFIES_ATOM_PATTERN.match(symbol)
+    if m is None:
+        return None
+    bond_char, isotope, element, chirality, h_count, charge = m.groups()
+
+    if symbol[1 + len(bond_char):-1] in ORGANIC_SUBSET:
+        atom_fac = functools.partial(Atom, element=element, is_aromatic=False)
+        return smiles_to_bond(bond_char), atom_fac
+
+    isotope = None if (isotope == "") else int(isotope)
+    if element not in ELEMENTS:
+        return None
+    chirality = None if (chirality == "") else chirality
+
+    s = h_count
+    if s == "":
+        h_count = 0
+    else:
+        h_count = int(s[1:])
+
+    s = charge
+    if s == "":
+        charge = 0
+    else:
+        charge = int(s[1:])
+        charge *= 1 if (s[0] == "+") else -1
+
+    atom_fac = functools.partial(
+        Atom,
+        element=element,
+        is_aromatic=False,
+        isotope=isotope,
+        chirality=chirality,
+        h_count=h_count,
+        charge=charge
+    )
+
+    return smiles_to_bond(bond_char), atom_fac
+
+
+def _build_atom_cache():
+    cache = dict()
+    common_symbols = [
+        "[#C+1]", "[#C-1]", "[#C]", "[#N+1]", "[#N]", "[#O+1]", "[#P+1]",
+        "[#P-1]", "[#P]", "[#S+1]", "[#S-1]", "[#S]", "[=C+1]", "[=C-1]",
+        "[=C]", "[=N+1]", "[=N-1]", "[=N]", "[=O+1]", "[=O]", "[=P+1]",
+        "[=P-1]", "[=P]", "[=S+1]", "[=S-1]", "[=S]", "[Br]", "[C+1]", "[C-1]",
+        "[C]", "[Cl]", "[F]", "[H]", "[I]", "[N+1]", "[N-1]", "[N]", "[O+1]",
+        "[O-1]", "[O]", "[P+1]", "[P-1]", "[P]", "[S+1]", "[S-1]", "[S]"
+    ]
+
+    for symbol in common_symbols:
+        cache[symbol] = _process_atom_selfies_no_cache(symbol)
+    return cache
+
+
+def _build_branch_cache():
+    cache = dict()
+    for L in range(1, 4):
+        for bond_char in ["", "=", "#"]:
+            symbol = "[{}Branch{}]".format(bond_char, L)
+            cache[symbol] = (smiles_to_bond(bond_char)[0], L)
+    return cache
+
+
+def _build_ring_cache():
+    cache = dict()
+    for L in range(1, 4):
+        # [RingL], [=RingL], [#RingL]
+        for bond_char in ["", "=", "#"]:
+            symbol = "[{}Ring{}]".format(bond_char, L)
+            order, stereo = smiles_to_bond(bond_char)
+            cache[symbol] = (order, L, (stereo, stereo))
+
+        # [-/RingL], [\/RingL], [\-RingL], ...
+        for lchar, rchar in itertools.product(["-", "/", "\\"], repeat=2):
+            if lchar == rchar == "-":
+                continue
+            symbol = "[{}{}Ring{}]".format(lchar, rchar, L)
+            order, lstereo = smiles_to_bond(lchar)
+            order, rstereo = smiles_to_bond(rchar)
+            cache[symbol] = (order, L, (lstereo, rstereo))
+    return cache
+
+
+_PROCESS_ATOM_CACHE = _build_atom_cache()
+
+_PROCESS_BRANCH_CACHE = _build_branch_cache()
+
+_PROCESS_RING_CACHE = _build_ring_cache()
